1fb02cd292302c1ad8aefb5e0269278c8d6d263e
Stephan van Rooij <stephan@svrooij.nl>
2013-10-16 12:52:58 +0200

    PingPresence => added state device
    I added a state device that can be used in your rules. So you can
    create a rule that only fires if everyone is home. Or more inportantly
    if there is no one home.

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ninja-presence-ping
 ===================
 
-This is a ping presence driver for your own Ninja Block (ninjablocks.com)
+This is a ping presence driver for your own Ninja Block (http://ninjablocks.com)
 
+To install the easy way you'll need the ninja toolbelt. (https://github.com/ninjablocks/ninja-toolbelt for instructions)
 install commando: ninja install https://github.com/svrooij/ninja-presence-ping
 
-You should edit the index.js file to include the hosts it should ping
+You should edit the index.js file to include the hosts it should ping. It is high up my list to enable configuration with the settings!!
+
+This driver will create two devices, one with the raw data as devices respond to a ping. And one generic state device with three states, 'NobodyHome' , 'SomeoneHome' , 'EverybodyHome'. So you should be able to use those in your rules!!
 
 For everyone who likes this driver, you can also use this widget (https://gist.github.com/svrooij/6974775)
--- index.js
+++ index.js
@@ -9,9 +9,21 @@ Presence.prototype.name = 'Presence - Lan Ping';
 
 Presence.prototype.init = function() {
 	//you should edit this line for devices you would like to ping.
-	this._hosts = ['192.168.96.160','192.168.96.161'];
+	this._hosts = ['iPhone-Stephan','iPhone-Alrieke'];
 	
 	this._ping = require ("ping");
+	
+// 	//Print default settings
+// 	this._app.log.info('Scan delay: '+this._opts.scanDelay);
+// 	this._app.log.info('Timeout: '+this._opts.timeout);
+	
+	this._opts.scanDelay = 20*1000;
+	this._opts.timeout = 5*60*1000;
+	
+	//Print current settings.
+	this._app.log.info('Scan delay: '+this._opts.scanDelay);
+	this._app.log.info('Timeout: '+this._opts.timeout);
+	this._state = 'NobodyHome';
 }
 
 Presence.prototype.scan = function() {
@@ -27,18 +39,71 @@ Presence.prototype.scan = function() {
     		//Fire see, this should notify the system.
     		self.see({
     			name: host,
-    			id: host,
+    			id: host.replace('.','_'), //dots should not be just in the ID, so we replace it.
     			distance: 99
     		});
     	} else {//not responding to ping
     		//write something to the log
-    		self._app.log.info('Ping => ' + host +' is not responding.');
+    		//self._app.log.info('Ping => ' + host +' is not responding.');
     	}
+    	
+    	self.updateState();
     	
     });
   });
   
+  
+    
   //Tell the system the scan is complete
   self.scanComplete();
 
+};
+
+//I know this actually should be in the base driver, but this is the easiest way for now.
+Presence.prototype.updateState = function(){
+	var self = this;
+  	
+  	//Get the number of objects in the timeout list.
+  	//Device get added and removed by the ninja-presence-base driver
+  	var aantal = Object.keys(self._timeouts).length;
+  	self._app.log.info('Ping => Number device online: ' + aantal);
+  	
+  	
+  	if(aantal == 0)
+  {
+  	self.emitState('NobodyHome');
+  } else if( aantal == self._hosts.length) 
+  {
+  	self.emitState('EverybodyHome');
+  } else 
+  {
+  	self.emitState('SomeoneHome');
+  }
+	
+	
+};
+
+//I know this actually should be in the base driver, but this is the easiest way for now.
+Presence.prototype.emitState = function(newState){
+	var self = this;
+	
+	//Check if the state has change since the last time it fired.
+	//So we don't send out a 'new' state if it is the same as the last state.
+	if(self._state != newState) {
+		//Remember the old ID
+		var tempD = self.D;
+		//Set the Driver ID to 244 (generic state driver)
+		self.D = 244;
+
+  		//Log the state change
+  		self._app.log.info('Ping => State changed from '+ self._state + ' to '+newState);
+  		//Save the new state so we can compare it.
+  		self._state = newState;
+  		
+  		//Send the new state to the Cloud
+  		self.emit('data' ,newState);
+  		
+  		//Set the driver ID back to the regular ID
+  		self.D = tempD;
+	}
 };
