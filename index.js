c116ec439027cdce322502b378cd4beb64431ce9
Stephan van Rooij <stephan@svrooij.nl>
2013-10-20 15:43:15 +0200

    PingPresence => Wrong name for ping
    The wrong name would be send by driver if more then one device would be
    pinged and one did not respond in time.
    And I did some clean up of the old code (now moved to the
    ninja-presence-base driver)

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ Presence.prototype.init = function() {
 	this._app.log.info('Ping => Hosts: ',this._opts.hosts);
 	this._app.log.info('Ping => Scan delay: '+this._opts.scanDelay);
 	this._app.log.info('Ping => Timeout: '+this._opts.timeout);
-	//this._state = 'NobodyHome';
-	
-	//self.emitCurrentState();
 	
 }
 
@@ -35,8 +32,20 @@ Presence.prototype.scan = function() {
   self._app.log.info('Ping => Start pinging hosts');
   for(var ip in self._opts.hosts) {
   	var host = self._opts.hosts[ip];
-  	self._app.log.info('Ping => Ping host: ', host);
-  	self._ping.sys.probe(host.ip,function(isAlive) {
+  	self.pingHost(host);
+  	
+  }
+    
+  //Tell the system the scan is complete
+  self.scanComplete();
+
+};
+
+//Here we ping the host. Host is an object with a name and an ipaddress
+Presence.prototype.pingHost = function(host){
+	var self = this;
+	self._app.log.info('Ping => Ping host: ', host);
+	self._ping.sys.probe(host.ip,function(isAlive) {
   		if(isAlive) {
   			self._app.log.info('Ping => ' + host.name +' is up.');
   			self.see({
@@ -48,46 +57,13 @@ Presence.prototype.scan = function() {
   		
   		}
   	});
-  }
-/*  //foreach host in the array hosts
-  self._hosts.forEach(function(host){
-    self._ping.sys.probe(host, function(isAlive){
-    	if(isAlive) { //If it is online
-    		//Write something to log
-    		self._app.log.info('Ping => ' + host +' is up.');
-    		
-    		//Fire see, this should notify the system.
-    		self.see({
-    			name: host,
-    			id: host.replace('.','_'), //dots should not be just in the ID, so we replace it.
-    			distance: 99
-    		});
-    	} else {//not responding to ping
-    		//write something to the log
-    		//self._app.log.info('Ping => ' + host +' is not responding.');
-    	}
-    	
-    	//self.updateState();
-    	
-    });
-  });
-  
-  //This is added because the dashboard loses the state after a timeout.
-  //Better send the status twice then not at all.
-  //self.emitCurrentState();
-  */
-    
-  //Tell the system the scan is complete
-  self.scanComplete();
+}
 
-};
 
 Presence.prototype.config = function(rpc,cb) {
 
   var self = this;
   
- 
-
 	if (!rpc) {
 		return cb(null,{"contents":[
 			{ "type": "paragraph", "text": "Welcome to the Ping Presence driver!"},
@@ -98,7 +74,9 @@ Presence.prototype.config = function(rpc,cb) {
 			{ "type":"close", "text":"Close"}
 		]});
 	}
-	 self._app.log.info('Settings', rpc.method, rpc);
+	
+	self._app.log.info('Settings', rpc.method, rpc);
+	
 	switch (rpc.method) {
 		case 'addModal':
     		return cb(null, {
@@ -116,7 +94,6 @@ Presence.prototype.config = function(rpc,cb) {
       var devOptions = {"ip": rpc.params.ip, "name": rpc.params.name}
       self._opts.hosts[rpc.params.ip] = devOptions;
       self.save();
-      //self.add(devOptions);
       cb(null, {
         "contents": [
           { "type":"paragraph", "text":"Device at " + rpc.params.ip + " with options (name : " + rpc.params.name + ") will be pinged starting today."},
@@ -140,7 +117,6 @@ Presence.prototype.config = function(rpc,cb) {
       self._opts.scanDelay = (rpc.params.scanDelay *1000);
       self._opts.timeout = (rpc.params.timeout *1000);
       self.save();
-      //self.add(devOptions);
       cb(null, {
         "contents": [
           { "type":"paragraph", "text":"New settings saved"},
@@ -154,67 +130,3 @@ Presence.prototype.config = function(rpc,cb) {
       self._app.log.error('Unknown rpc method', rpc.method, rpc);
   }
 };
-
-
-/*
-//I know this actually should be in the base driver, but this is the easiest way for now.
-Presence.prototype.updateState = function(){
-	var self = this;
-  	
-  	//Get the number of objects in the timeout list.
-  	//Device get added and removed by the ninja-presence-base driver
-  	var aantal = Object.keys(self._timeouts).length;
-  	self._app.log.info('Ping => Number device online: ' + aantal);
-  	
-  	
-  	if(aantal == 0)
-  {
-  	self.actuateState('NobodyHome');
-  } else if( aantal == self._hosts.length) 
-  {
-  	self.actuateState('EverybodyHome');
-  } else 
-  {
-  	self.actuateState('SomeoneHome');
-  }
-	
-	
-};
-
-Presence.prototype.emitCurrentState = function(){
-	var self = this;
-	self.emitState(self._state);
-};
-
-//I know this actually should be in the base driver, but this is the easiest way for now.
-Presence.prototype.emitState = function(state){
-	var self = this;
-	
-	//Remember the old ID
-	var tempD = self.D;
-	//Set the Driver ID to 244 (generic state driver)
-	self.D = 244;
-	
-	//Send the new state to the Cloud
-	self.emit('data' ,state);
-  		
-	//Set the driver ID back to the regular ID
-	self.D = tempD;
-	
-};
-
-Presence.prototype.actuateState = function(newState) {
-	var self = this;
-	//Check if the state has change since the last time it fired.
-	//So we don't send out a 'new' state if it is the same as the last state.
-	if(self._state != newState)
-	{
-	
-		//Log the state change
-  		self._app.log.info('Ping => State changed from '+ self._state + ' to '+newState);
-  		//Save the new state so we can compare it.
-  		self._state = newState;
-  		self.emitState(newState);
-  	}
-}
-*/
