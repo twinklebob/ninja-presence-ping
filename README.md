cfd524b14082d28508bd489afe33c9125d5ca58e
Stephan van Rooij <stephan@svrooij.nl>
2013-10-20 12:59:28 +0200

    PingPresence => Added settings
    Settings are now added. You can add more hosts with the normal driver
    config screens

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ To install the easy way you'll need the ninja toolbelt. (https://github.com/ninj
 
 install commando: `ninja install https://github.com/svrooij/ninja-presence-ping`
 
-You should edit the index.js file to include the hosts it should ping. It is high up my list to enable configuration with the settings!!
-
 This driver will create two devices, one with the raw data as devices respond to a ping. And one generic state device with three states, 'NobodyHome' , 'SomeoneHome' , 'EverybodyHome'. 
 So you should be able to use those in your rules!!
 
@@ -22,6 +20,7 @@ To-do
 
 - [x] Create a [gist](https://gist.github.com/svrooij/6974775) for display
 - [x] Add state device for using the presence in the rules engine
-- [ ] Enable configuration of host trough the settings, instead of the 'edit the index.js file' way
-- [ ] Rewrite the sub driver
+- [x] Enable configuration of host trough the settings, instead of the 'edit the index.js file' way
+- [x] Rewrite the sub driver
+- [x] Moved main state logic to ninja-presence-base
 - [ ] Add a device per host, for even better control of the rules
--- index.js
+++ index.js
@@ -9,7 +9,8 @@ Presence.prototype.name = 'Presence - Lan Ping';
 
 Presence.prototype.init = function() {
 	//you should edit this line for devices you would like to ping.
-	this._hosts = ['iPhone-Stephan','iPhone-Alrieke'];
+	//this._hosts = ['192.168.96.160','192.168.96.161'];
+	this._opts.hosts = this._opts.hosts || {};
 	
 	this._ping = require ("ping");
 	
@@ -17,22 +18,36 @@ Presence.prototype.init = function() {
 // 	this._app.log.info('Scan delay: '+this._opts.scanDelay);
 // 	this._app.log.info('Timeout: '+this._opts.timeout);
 	
-	this._opts.scanDelay = 20*1000;
-	this._opts.timeout = 5*60*1000;
+	this._opts.scanDelay = this._opts.scanDelay || 20*1000; //Default scanDelay
+	this._opts.timeout = this._opts.timeout || 5*60*1000; //Default timeout
 	
 	//Print current settings.
 	this._app.log.info('Scan delay: '+this._opts.scanDelay);
 	this._app.log.info('Timeout: '+this._opts.timeout);
-	this._state = 'NobodyHome';
+	//this._state = 'NobodyHome';
 	
-	self.emitCurrentState();
+	//self.emitCurrentState();
 	
 }
 
 Presence.prototype.scan = function() {
   var self = this;
-
-  //foreach host in the array hosts
+  for(var ip in self._opts.hosts) {
+  	var host = self._opts.hosts[ip];
+  	self._ping.sys.probe(host.ip,function(isAlive) {
+  		if(isAlive) {
+  			self._app.log.info('Ping => ' + host.name +' is up.');
+  			self.see({
+  				name : host.name,
+  				id : host.ip.replace('.','_'),
+  				distance:99
+  			});
+  		} else {
+  		
+  		}
+  	});
+  }
+/*  //foreach host in the array hosts
   self._hosts.forEach(function(host){
     self._ping.sys.probe(host, function(isAlive){
     	if(isAlive) { //If it is online
@@ -50,21 +65,93 @@ Presence.prototype.scan = function() {
     		//self._app.log.info('Ping => ' + host +' is not responding.');
     	}
     	
-    	self.updateState();
+    	//self.updateState();
     	
     });
   });
   
   //This is added because the dashboard loses the state after a timeout.
   //Better send the status twice then not at all.
-  self.emitCurrentState();
-  
+  //self.emitCurrentState();
+  */
     
   //Tell the system the scan is complete
   self.scanComplete();
 
 };
 
+Presence.prototype.config = function(rpc,cb) {
+
+  var self = this;
+
+	if (!rpc) {
+		return cb(null,{"contents":[
+			{ "type": "paragraph", "text": "Welcome to the Ping Presence driver!"},
+			{ "type": "submit", "name": "General Settings", "rpc_method": "genSettings" },
+			{ "type": "paragraph", "text": "Before it works you have to add some devices!"},
+			{ "type": "submit", "name": "Add device to ping", "rpc_method": "addModal" },
+			
+			{ "type":"close", "text":"Close"}
+		]});
+	}
+
+	switch (rpc.method) {
+		case 'addModal':
+    		cb(null, {
+        	"contents":[
+          		{ "type": "paragraph", "text":"Please enter the IP address or the hostname and a nickname of the device to be pinged"},
+          		{ "type": "input_field_text", "field_name": "ip", "value": "", "label": "IP/Hostname", "placeholder": "x.x.x.x", "required": true},
+          		{ "type": "input_field_text", "field_name": "name", "value": "", "label": "Name", "placeholder": "Device X", "required": true},
+          		{ "type": "submit", "name": "Add", "rpc_method": "add" },
+          		{ "type":"close", "text":"Cancel"}
+        	]
+      	});
+      	break;
+    case 'add':
+      var devOptions = {"ip": rpc.params.ip, "name": rpc.params.name}
+      self._opts.hosts[rpc.params.ip] = devOptions;
+      self.save();
+      //self.add(devOptions);
+      cb(null, {
+        "contents": [
+          { "type":"paragraph", "text":"Device at " + rpc.params.ip + " with options (name : " + rpc.params.name + ") will be pinged starting today."},
+          { "type":"close", "text":"Close"}
+        ]
+      });
+      break;
+    case 'genSettings':
+      cb(null, {
+        "contents": [
+          { "type":"paragraph", "text":"Set the general settings of the Ping Presence driver here according to your wishes"},
+          { "type": "input_field_text", "field_name": "scanDelay", "value": (self._opts.scanDelay/1000), "label": "Scan delay in seconds", "placeholder": "20", "required": true},
+          { "type": "input_field_text", "field_name": "timeout", "value": (self._opts.timeout/1000), "label": "Timeout in seconds", "placeholder": "300", "required": true},
+          { "type": "submit", "name": "Save", "rpc_method": "saveSettings" },
+          { "type":"close", "text":"Cancel"}
+        ]
+      });
+      break;
+  	case 'saveSettings':
+      console.log('New scandelay '+rpc.params.scanDelay+' and timeout '+rpc.params.timeout);
+      self._opts.scanDelay = (rpc.params.scanDelay *1000);
+      self._opts.timeout = (rpc.params.timeout *1000);
+      self.save();
+      //self.add(devOptions);
+      cb(null, {
+        "contents": [
+          { "type":"paragraph", "text":"New settings saved"},
+          { "type":"paragraph", "text":"Scan Delay => " + rpc.params.scanDelay + " Timeout " + rpc.params.timeout},
+          { "type":"paragraph", "text":"Timeout => " + rpc.params.timeout},
+          { "type":"close", "text":"Close"}
+        ]
+      });
+      break;
+    default:
+      log('Unknown rpc method', rpc.method, rpc);
+  }
+};
+
+
+/*
 //I know this actually should be in the base driver, but this is the easiest way for now.
 Presence.prototype.updateState = function(){
 	var self = this;
@@ -125,3 +212,4 @@ Presence.prototype.actuateState = function(newState) {
   		self.emitState(newState);
   	}
 }
+*/
